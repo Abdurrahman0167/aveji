470f14c79b59bbd9f7efcd0ff96cc23958db2786
Abdurrahman <96380888+Abdurrahman0167@users.noreply.github.com>
2025-07-18 16:44:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+      },
+});
